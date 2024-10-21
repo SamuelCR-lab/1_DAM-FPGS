e572cf4f8d384c85234ce7cfc4af175c4c370ff7
SamuelACR <samuelcarias1@gmail.com>
2024-10-21 14:12:26 +0200

    rama1

--- miProyecto/script.c
+++ miProyecto/script.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-<<<<<<< HEAD
-int main(){
-printf("Hola Git\n");
-return 0;
-}
-=======
 int main (){
 printf("Hola, Git\n");
 printf ("Samuel Carías Ramos\n");
 return 0;
 }
 
->>>>>>> agregar-mensaje
