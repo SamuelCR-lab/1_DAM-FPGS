303aecb5a74ee78f188615a86aa594855af9e5d2
SamuelCR-lab <fp.scariasramos@salesianosdosa.com>
2024-10-23 00:43:32 +0200

    Str no me esta funcionando bien

--- Programacion/PracticasClase/P5_CadenaCaracteres/Ejercicio3.c
+++ Programacion/PracticasClase/P5_CadenaCaracteres/Ejercicio3.c
@@ -7,16 +7,27 @@
  */
 
 int main (){
-	char str1[50];
+	char str1[20];
 	int cantidad;
-	char str2[50];
+	char str2[20];
 	printf("Introduce una palabra: ");
-	scanf("%s",str1);//No hace falta el & debido a que una cadena de caracteres de valores str ya tiene usignado un espacio de memoria reservado para los digitos en [] por lo que solo de llamar a la memoria str sabe lo que se ha guardado
-	printf("Introduce una palabra: ");
-	scanf("%s",str2);//No hace falta el & debido a que una cadena de caracteres de valores str ya tiene usignado un espacio de memoria reservado para los digitos en [] por lo que solo de llamar a la memoria str sabe lo que se ha guardado
-	if (str1 == str2) {
-		printf("Es la misma palabra %s y %s",str1,str2);
+	scanf(" %s",str1);//No hace falta el & debido a que una cadena de caracteres de valores str ya tiene usignado un espacio de memoria reservado para los digitos en [] por lo que solo de llamar a la memoria str sabe lo que se ha guardado
+	printf("Introduce la misma palabra: ");
+	scanf(" %s",str2);
+	if (str1 == str2){
+		printf("%s y %s son la misma palabra\n",str1,str2);
+	}else{
+		printf("%s y %s No son la misma palabra\n",str1,str2);
 	}
+	if (str1 > str2){
+		printf("La cadena 1 %s es mayor que la cadena 2 %s.\n",str1,str2);
+	}else if (str1 < str2){
+		printf("La cadena 2 %s es mayor que la cadena 1 %s.\n",str2,str1);
+
+	}else{
+		printf("%s y %s son la misma palabra\n",str1,str2);
+	}
+
 
 return 0;
 }
