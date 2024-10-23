3fa5dd879ef7173b95d543db11e0194607979462
SamuelCR-lab <fp.scariasramos@salesianosdosa.com>
2024-10-23 20:44:18 +0200

    inicio 9

---
+++ Programacion/PracticasClase/5 - Bucles/EjercicioBucle9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *
+ *	Autor: Samuel Carías
+ *	Los 10 numeros de Figonacci son 0 1 1 2 3 5 8 13 21 44 65 109...
+ */
+int main (){  
+	int  i, num, anterior, anterior_del_anterior;
+		printf("Introduce un número cualquiera: ");
+		scanf(" %d",&num);
+		for (i = 1; i <= 10; i++){
+			
+
+
+			}
+		}
+		printf("El numero %d tiene %d divisores\n",num,divisor);
+return 0;
+}
