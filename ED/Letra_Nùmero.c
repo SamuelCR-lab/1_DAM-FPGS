08d1da2b1c12ccf5f03b1ebad365e824c2385612
SamuelCR-lab <fp.scariasramos@salesianosdosa.com>
2024-10-27 21:58:13 +0100

    Create Letra_Nùmero.c
    Primer commit remoto

---
+++ ED/Letra_Nùmero.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+  int main() {
+    char caracter;
+      printf("Introduce un carácter cualquiera: ");
+      scanf(" %c", &caracter);
+      if (caracter >= 0 && caracter <=9) {
+      printf("Es un número\n");
+      }else if ((caracter >= 'A' && caracter <= 'Z')||(caracter >= 'a' && caracter <= 'z')) {
+      printf("Es una letra\n");
+      }else {
+      printf("No es ni un número ni una letra\n");
+      }
+    return 0;
+}
