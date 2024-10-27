dbc5867cfd3cc70e2924fce15e0be3f74ec0df37
SamuelCR-lab <fp.scariasramos@salesianosdosa.com>
2024-10-27 21:26:50 +0100

    Update and rename documentoExtra to documentoExtra.c
    Cambios dentro del documentoExtra para que una funcion diga si es par o impar si el numero es par o impar

---
+++ ED/documentoExtra.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+  int main() {
+  int num;
+    printf("Introduce un número: ");
+    scanf("%d", &num);
+    if (num % 2 == 0) {
+    printf("El número es par\n");
+    } else {
+    printf("El número es impar\n");
+    }
+return 0;' >> documentoExtra
+}
